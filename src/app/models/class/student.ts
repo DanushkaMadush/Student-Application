096ddfdc4f7dc90f0cb3097b5ac9ac3692e77826
danushkacodeolima <danushka@codeolima.com>
2024-10-05 20:57:00 +0530

    student class created

---
+++ src/app/models/class/student.ts
@@ -0,0 +1,25 @@
+export class Student {
+    studentID : number;
+    firstName : string;
+    lastName :string;
+    email : string;
+    phone : string;
+    address : string;
+    country : string;
+    institute : string;
+    intake : string;
+    courseTitle : string;
+
+    constructor () {
+    this.studentID = 0;
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.phone = '';
+    this.address = '';
+    this.country = '';
+    this.institute = '';
+    this.intake = '';
+    this.courseTitle = '';
+    }
+}
