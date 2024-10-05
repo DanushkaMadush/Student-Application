39ca128a17db4c74addea55f598d6cbe28360fed
danushkacodeolima <danushka@codeolima.com>
2024-10-05 19:58:07 +0530

    admin route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { StudentComponent } from './components/student/student/student.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
     {
         path : 'student',
         component : StudentComponent
+    },
+
+    {
+        path : 'admin',
+        component : AdminComponent
     }
 ];
