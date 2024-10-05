4eadc4fc437c2f74d6bb53b5e6f94a9c6f5ec8e4
danushkacodeolima <danushka@codeolima.com>
2024-10-05 20:16:02 +0530

    admin component designed

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { StudentComponent } from '../student/student/student.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet , StudentComponent , AdminComponent , RouterLink],
+  imports: [RouterOutlet , StudentComponent , AdminComponent , RouterLink , FormsModule , CommonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-
+  isLoader : boolean = true;
 }
