a22a1a78f4601d1ff4d47e10286de9bfd737b858
danushkacodeolima <danushka@codeolima.com>
2024-10-08 18:41:35 +0530

    added mat lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentComponent } from './components/student/student/student.component';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , StudentComponent],
+  imports: [RouterOutlet , StudentComponent , MatIconModule , MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
