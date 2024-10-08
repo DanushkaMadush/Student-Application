599639882fe640713546027a244edfba53c2dcff
danushkacodeolima <danushka@codeolima.com>
2024-10-08 05:59:18 +0530

    API integrated

--- src/app/components/student/student/student.component.ts
+++ src/app/components/student/student/student.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component , inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Student } from '../../../models/class/student';
+import { StudentService } from '../../../services/student.service';
+import { APIResponseModel } from '../../../models/response';
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './student.component.html',
   styleUrl: './student.component.css'
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit {
+  studentObj : Student = new Student();
+  studentList : Student[] = [];
+  studentService = inject(StudentService)
+
+  ngOnInit(): void {
+    
+  }
+
+  loadStudent () {
+    this.studentService.getAllStudents().subscribe((res:APIResponseModel) =>{
+      this.studentList = res.data;
+    })
+  }
+
+  onSaveStudent() {
+    debugger;
+    this.studentService.addStudent(this.studentObj).subscribe((res:APIResponseModel) => {
+      if(res.result) {
+        alert("Student created success")
+      } else {
+        alert(res.message)
+      }
+    })
+  }
 
 }
