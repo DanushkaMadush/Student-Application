40d01ee6329f58202c71422296cbffe581160471
danushkacodeolima <danushka@codeolima.com>
2024-10-08 05:42:53 +0530

    added env

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../models/class/student';
+import { environment } from '../../environments/environment.development';
+import { APIResponseModel } from '../models/response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private http : HttpClient) { }
+
+  getAllStudents ():Observable<APIResponseModel> {
+    return this.http.get<APIResponseModel>(environment.API_URL + "Student");
+  }
+
+  addStudent (obj : Student):Observable<APIResponseModel> {
+    return this.http.post<APIResponseModel>(environment.API_URL + "Student" , obj);
+  }
+
+}
